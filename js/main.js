41fc81863afee6f4d77b288000ddaff41daa413a
vyvienne <vivientan.wy@gmail.com>
2025-09-10 22:08:55 +0800

    update jsx and css file

--- js/main.js
+++
@@ -1,9 +0,0 @@
-import { OrderType } from "./enums/orderType.js";
-import { SystemManager } from "./objects/systemManager.js";
-
-const system = new SystemManager();
-
-document.getElementById("add-normal-order").onclick = () => system.addOrder(OrderType.NORMAL);
-document.getElementById("add-vip-order").onclick = () => system.addOrder(OrderType.VIP);
-document.getElementById("add-bot").onclick = () => system.addBot();
-document.getElementById("remove-bot").onclick = () => system.removeBot();
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { OrderType } from "./js/enums/orderType.js";
+import { SystemManager } from "./js/objects/systemManager.js";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const system = new SystemManager();
+
+  const handleNormalOrder = () => {
+    system.addOrder(OrderType.NORMAL);
+  };
+
+  const handleVIPOrder = () => {
+    system.addOrder(OrderType.VIP);
+  };
+
+  const handleAddBot = () => {
+    system.addBot();
+  };
+
+  const handleRemoveBot = () => {
+    system.removeBot();
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="app">
+        <center>
+          <h3>COOKINGBOT ORDER SYSTEM</h3>
+        </center>
+        <div id="system-container">
+          <div id="container">
+            <div>
+              <div id="pending"></div>
+              <center>
+                <h4>PENDING ORDERS</h4>
+                <button id="add-normal-order" onClick={handleNormalOrder}>NEW NORMAL ORDER</button>
+                <br />
+                <button id="add-vip-order" onClick={handleVIPOrder}>NEW VIP ORDER</button>
+              </center>
+            </div>
+
+            <div>
+              <div id="complete"></div>
+              <center>
+                <h4>COMPLETED ORDERS</h4>
+              </center>
+            </div>
+
+            <div>
+              <div id="bots"></div>
+              <center>
+                <h4>AVAILABLE BOTS</h4>
+                <button id="add-bot" onClick={handleAddBot}>+ BOT</button>
+                <br />
+                <button id="remove-bot" onClick={handleRemoveBot}>- BOT</button>
+              </center>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
 export default App
